18c611f9e86ae8780e327f196a3c5e88e7c5a6d3
Joshua Tucker <the1jtux@gmail.com>
2021-12-02 02:38:11 -0500

    Day 2 Task 1 Complete

---
+++ AdventOfCode.Day2/Program.cs
@@ -0,0 +1,30 @@
+﻿//-- Task 1
+// Get input from Input.txt (added to .csproj)
+string input = new StreamReader("Input.txt").ReadToEnd();
+
+var commands = input.Split('\n')
+    .Select(c => c.Split(' '))
+    .Select(c => new { Direction = c[0], Value = int.Parse(c[1]) })
+    .ToList();
+
+int horizontal = 0, vertical = 0;
+foreach (var command in commands)
+{
+    switch (command.Direction)
+    {
+        case "forward":
+            horizontal += command.Value;
+            break;
+        case "down":
+            vertical += command.Value;
+            break;
+        case "up":
+            vertical -= command.Value;
+            break;
+        default:
+            Console.WriteLine(command.Direction + " " + command.Value);
+            break;
+    }
+}
+
+Console.WriteLine(horizontal + " " + vertical + " " + horizontal * vertical);
