d56bb6a7216bb504c3d4b110122b2ef72aa7d7e9
Joshua Tucker <the1jtux@gmail.com>
2021-12-02 02:07:45 -0500

    Day 1 Task 1 Complete

--- AdventOfCode.Day1/Program.cs
+++ AdventOfCode.Day1/Program.cs
@@ -1,2 +1,31 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Get input from Input.txt (added to .csproj)
+string input = new StreamReader("Input.txt").ReadToEnd();
+
+// Split the input string into seperate values and parse them to integers
+List<int> depths = input
+    .Split('\n')
+    .Select(depth => int.Parse(depth))
+    .ToList();
+
+// Set the first depth as the first value of the list
+int previous = depths[0];
+
+// As the first number hasn't increased, start at 0 iterations
+int depthIncreaseCount = 0;
+
+// Skip the first value but check the rest
+for (int i = 1; i < depths.Count; i++)
+{
+    // Get the new depth
+    int depth = depths[i];
+
+    // Compare the two depths, if it increases increase the count
+    if (depth > previous)
+        depthIncreaseCount++;
+
+    // Save current depth as previous for next iteration
+    previous = depth;
+}
+
+Console.WriteLine(depthIncreaseCount);
