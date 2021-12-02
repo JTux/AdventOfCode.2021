119c882cef8951743b1be8437cef535b502bd478
Joshua Tucker <the1jtux@gmail.com>
2021-12-02 02:43:49 -0500

    Day 2 Task 2 Complete

--- AdventOfCode.Day2/Program.cs
+++ AdventOfCode.Day2/Program.cs
@@ -21,10 +21,31 @@ foreach (var command in commands)
         case "up":
             vertical -= command.Value;
             break;
-        default:
-            Console.WriteLine(command.Direction + " " + command.Value);
+    }
+}
+
+Console.WriteLine(horizontal + " + " + vertical + " = " + horizontal * vertical);
+
+//-- Task 2 - Might refactor later to be not so tall
+int aim = 0;
+
+horizontal = 0;
+vertical = 0;
+foreach(var command in commands)
+{
+    switch (command.Direction)
+    {
+        case "forward":
+            horizontal += command.Value;
+            vertical += aim * command.Value;
+            break;
+        case "down":
+            aim += command.Value;
+            break;
+        case "up":
+            aim -= command.Value;
             break;
     }
 }
 
-Console.WriteLine(horizontal + " " + vertical + " " + horizontal * vertical);
+Console.WriteLine(horizontal + " + " + vertical + " = " + horizontal * vertical);
